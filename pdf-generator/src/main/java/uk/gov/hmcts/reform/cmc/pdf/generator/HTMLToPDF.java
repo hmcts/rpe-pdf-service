9df44f06a74b1c2abc53721852e3777d37e0cd33
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-05-31 16:21:08 +0100

    Factor out PDFGenerator class

--- pdf-generator/src/main/java/uk/gov/hmcts/reform/cmc/pdf/generator/HTMLToPDF.java
+++ pdf-generator/src/main/java/uk/gov/hmcts/reform/cmc/pdf/generator/HTMLToPDF.java
@@ -1,45 +1,25 @@
 package uk.gov.hmcts.reform.cmc.pdf.generator;
 
-import org.xhtmlrenderer.pdf.ITextRenderer;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Map;
-import java.util.UUID;
 
 @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 public class HTMLToPDF {
 
     private HTMLTemplateProcessor templateProcessor;
+    private PDFGenerator pdfGenerator;
 
     public HTMLToPDF() {
-        this(new HTMLTemplateProcessor());
+        this(new HTMLTemplateProcessor(), new PDFGenerator());
     }
 
-    public HTMLToPDF(HTMLTemplateProcessor templateProcessor) {
+    public HTMLToPDF(HTMLTemplateProcessor templateProcessor, PDFGenerator pdfGenerator) {
         this.templateProcessor = templateProcessor;
+        this.pdfGenerator = pdfGenerator;
     }
 
     public byte[] generate(byte[] template, Map<String, Object> context) {
         String processedHtml = templateProcessor.process(template, context);
-        return convertToPDF(processedHtml);
-    }
-
-    private byte[] convertToPDF(String htmlString) {
-        try {
-            final File outputFile = File.createTempFile(UUID.randomUUID().toString(), ".pdf");
-            OutputStream outputStream = new FileOutputStream(outputFile);
-            ITextRenderer renderer = new ITextRenderer();
-            renderer.setDocumentFromString(htmlString);
-            renderer.layout();
-            renderer.createPDF(outputStream, true);
-            return Files.readAllBytes(Paths.get(outputFile.toURI()));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return pdfGenerator.generateFrom(processedHtml);
     }
 
 }
---
+++ pdf-generator/src/main/java/uk/gov/hmcts/reform/cmc/pdf/generator/PDFGenerator.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.cmc.pdf.generator;
+
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+public class PDFGenerator {
+
+    public byte[] generateFrom(String htmlString) {
+        try {
+            final File outputFile = File.createTempFile(UUID.randomUUID().toString(), ".pdf");
+            OutputStream outputStream = new FileOutputStream(outputFile);
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(htmlString);
+            renderer.layout();
+            renderer.createPDF(outputStream, true);
+            return Files.readAllBytes(Paths.get(outputFile.toURI()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
