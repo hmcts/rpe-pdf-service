4f2cf9b700c029f273013b33ea7869cfbafe7f26
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-06-02 16:07:57 +0200

    Remove unused import

--- pdf-generator/src/main/java/uk/gov/hmcts/reform/cmc/pdf/generator/exception/MalformedTemplateException.java
+++ pdf-generator/src/main/java/uk/gov/hmcts/reform/cmc/pdf/generator/exception/MalformedTemplateException.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.cmc.pdf.generator.exception;
 
-import org.xhtmlrenderer.util.XRRuntimeException;
-
 public class MalformedTemplateException extends RuntimeException {
 
     public MalformedTemplateException(String message, Throwable cause) {
