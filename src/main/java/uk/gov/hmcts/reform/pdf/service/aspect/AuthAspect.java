74001eb9e58fa374a641e60372cd1923a3b7496e
Donatas Martinkus <donatas.martinkus@hmcts.net>
2017-12-06 08:43:42 +0000

    Make aspect advice more concrete

--- src/main/java/uk/gov/hmcts/reform/pdf/service/aspect/AuthAspect.java
+++ src/main/java/uk/gov/hmcts/reform/pdf/service/aspect/AuthAspect.java
@@ -3,19 +3,47 @@ package uk.gov.hmcts.reform.pdf.service.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.reform.pdf.service.service.AuthService;
+
+import java.lang.annotation.Annotation;
 
 @Aspect
 @Component
 public class AuthAspect {
 
+    private boolean stop;
+
     @Autowired
     private AuthService authService;
 
-    @Before("execution(* uk.gov.hmcts.reform.pdf.service.endpoint.v2.*.*(String, ..))")
+    private void parseAndAuthenticate(RequestHeader header, String serviceAuthHeader) {
+        if (header.value().equals("ServiceAuthorization")) {
+            stop = true;
+            authService.authenticate(serviceAuthHeader);
+        }
+    }
+
+    @Before("execution(* uk.gov.hmcts.reform.pdf.service.endpoint.v2.*.*(" +
+        "@org.springframework.web.bind.annotation.RequestHeader (*), ..))")
     public void authenticate(JoinPoint jp) {
-        authService.authenticate((String) jp.getArgs()[0]);
+        stop = false;
+        Object[] args = jp.getArgs();
+        Annotation[][] annotations = ((MethodSignature) jp.getSignature()).getMethod().getParameterAnnotations();
+
+        for (int i = 0; i < annotations.length; i++) {
+            for (Annotation annotation : annotations[i]) {
+                if (annotation.annotationType() == RequestHeader.class) {
+                    parseAndAuthenticate((RequestHeader) annotation, (String) args[i]);
+                }
+            }
+
+            if (stop) {
+                break;
+            }
+        }
     }
 }
