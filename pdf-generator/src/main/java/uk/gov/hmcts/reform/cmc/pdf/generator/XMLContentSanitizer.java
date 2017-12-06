bb53750b9f2e3a7783b839b198f8ca637fe02766
Rafał Lewandowski <r.lewandowski@kainos.com>
2017-12-06 12:30:22 +0000

    Provide a XML content sanitizer class

---
+++ pdf-generator/src/main/java/uk/gov/hmcts/reform/cmc/pdf/generator/XMLContentSanitizer.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.cmc.pdf.generator;
+
+/**
+ * <p>Implemented according to <a href="https://www.w3.org/TR/xml/#charsets">https://www.w3.org/TR/xml/#charsets</a>.</p>
+ *
+ * <p>Decided to strip the <code>[#x10000-#x10FFFF]</code> supplementary plane characters as well since allowing them would make the
+ * code much more complex. This is because Java String is UTF-16 and does not support those as literals and I would
+ * need to operate on surrogate code pairs.</p>
+ *
+ *
+ * <p>For reference:
+ * <ul>
+ *   <li><a href="https://en.wikipedia.org/wiki/UTF-16#Description">UTF-16 Surrogate code points</a></li>
+ *   <li><a href="https://en.wikipedia.org/wiki/Plane_(Unicode)#Supplementary_Multilingual_Plane">Supplementary Multilingual Plane</a></li>
+ *   <li><a href="https://stackoverflow.com/questions/36954070/understanding-unicode-surrogate-blocks-noncharacters">What are Unicode surrogate blocks?</a></li>
+ * </ul></p>
+ */
+public class XMLContentSanitizer {
+
+    private static final String INVALID_XML_CHARS = "[^\\u0009\\u000A\\u000D\\u0020-\\uD7FF\\uE000-\\uFFFD]";
+
+    public String stripIllegalCharacters(String input) {
+        return input.replaceAll(INVALID_XML_CHARS, "");
+    }
+
+}
---
+++ pdf-generator/src/test/java/uk/gov/hmcts/reform/cmc/pdf/generator/XMLContentSanitizerTest.java
@@ -0,0 +1,67 @@
+package uk.gov.hmcts.reform.cmc.pdf.generator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XMLContentSanitizerTest {
+
+    private XMLContentSanitizer contentSanitizer;
+
+    @Before
+    public void beforeEachTest() {
+        contentSanitizer = new XMLContentSanitizer();
+    }
+
+    @Test
+    public void shouldReturnValidStringUnmodified() {
+        String input = "Valid string";
+        String output = contentSanitizer.stripIllegalCharacters(input);
+        assertThat(output).isEqualTo(input);
+    }
+
+    @Test
+    public void shouldStripAnIllegalFormFeedCharacterAndKeepTheRestOfInput() {
+        String input = "abc\u000Cdef";
+        String expected = "abcdef";
+        String output = contentSanitizer.stripIllegalCharacters(input);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    public void shouldKeepAllowedControlCharacters() {
+        String input = "\u0009\n\r";
+        String output = contentSanitizer.stripIllegalCharacters(input);
+        assertThat(output).isEqualTo(input);
+    }
+
+    @Test
+    public void shouldKeepAllowedCharacterFromBasicMultilingualPlaneEdgesBeforeSurrogateBlocks() {
+        String input = "\u0020\uD7FF";
+        String output = contentSanitizer.stripIllegalCharacters(input);
+        assertThat(output).isEqualTo(input);
+    }
+
+    @Test
+    public void shouldKeepAllowedCharacterFromBasicMultilingualPlaneEdgesAfterSurrogateBlocks() {
+        String input = "\uE000\uFFFD";
+        String output = contentSanitizer.stripIllegalCharacters(input);
+        assertThat(output).isEqualTo(input);
+    }
+
+    @Test
+    public void shouldStripSurrogateBlockCodePoint() {
+        String input = "\uD800";
+        String output = contentSanitizer.stripIllegalCharacters(input);
+        assertThat(output).isEqualTo("");
+    }
+
+    @Test
+    public void shouldStripFFFEAnfFFFFSpecialCharacters() {
+        String input = "\uFFFE\uFFFF";
+        String output = contentSanitizer.stripIllegalCharacters(input);
+        assertThat(output).isEqualTo("");
+    }
+
+}
