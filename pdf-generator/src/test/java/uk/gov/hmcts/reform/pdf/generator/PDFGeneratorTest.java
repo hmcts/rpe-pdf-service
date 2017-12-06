0d329cdf316c4170505cea0b1d55fc9e955ef9ed
Rafał Lewandowski <rafal_lewandowski@outlook.com>
2017-12-06 16:27:30 +0000

    ROC-2937: Strip illegal XML characters (#22)

---
+++ pdf-generator/src/test/java/uk/gov/hmcts/reform/pdf/generator/PDFGeneratorTest.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.pdf.generator;
+
+import org.junit.Test;
+import org.xml.sax.SAXParseException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
+public class PDFGeneratorTest {
+
+    private PDFGenerator pdfGenerator = new PDFGenerator();
+    private XMLContentSanitizer contentSanitizer = new XMLContentSanitizer();
+
+    @Test
+    public void shouldThrowSaxParseExceptionWhenGivenHTMLWithIllegalCharacters() {
+        String illegalHTML = ResourceLoader.loadString("/illegal-characters.html");
+
+        Throwable thrown = catchThrowable(() -> pdfGenerator.generateFrom(illegalHTML));
+
+        assertThat(thrown.getCause())
+            .isInstanceOf(SAXParseException.class)
+            .hasMessage("An invalid XML character (Unicode: 0xc) was found in the element content of the document.");
+    }
+
+    @Test
+    public void shouldProcessSuccessfullyAfterRunningIllegalHTMLThroughSanitizer() {
+        String illegalHTML = ResourceLoader.loadString("/illegal-characters.html");
+
+        Throwable thrown = catchThrowable(
+            () -> pdfGenerator.generateFrom(contentSanitizer.stripIllegalCharacters(illegalHTML))
+        );
+
+        assertThat(thrown).isNull();
+    }
+
+}
