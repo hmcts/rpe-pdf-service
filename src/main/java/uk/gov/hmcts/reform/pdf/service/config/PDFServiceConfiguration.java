a09f235ddea5c5ef736225d5ae4b359cd1857785
Donatas Martinkus <donatas.martinkus@hmcts.net>
2017-12-07 14:06:12 +0000

    Remove unnecessary Bean definition. Leftover during development process

--- src/main/java/uk/gov/hmcts/reform/pdf/service/config/PDFServiceConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pdf/service/config/PDFServiceConfiguration.java
@@ -1,25 +1,14 @@
 package uk.gov.hmcts.reform.pdf.service.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.pdf.generator.HTMLToPDFConverter;
-import uk.gov.hmcts.reform.pdf.service.client.S2SClient;
-import uk.gov.hmcts.reform.pdf.service.service.AuthorisationService;
 
 @Configuration
 public class PDFServiceConfiguration {
 
-    @Autowired
-    private S2SClient s2SClient;
-
     @Bean
     public HTMLToPDFConverter htmlToPdf() {
         return new HTMLToPDFConverter();
-    }
-
-    @Bean
-    public AuthorisationService authorisationService() {
-        return new AuthorisationService(s2SClient);
     }
 }
