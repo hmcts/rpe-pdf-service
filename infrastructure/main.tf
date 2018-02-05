313be38afab76c004520c21852cf78f08404843a
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-05 15:31:57 +0000

    Back to master

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 module "pdf-service-api" {
-  source = "git@github.com:contino/moj-module-webapp.git?ref=add-java-to-staging-slot"
+  source = "git@github.com:contino/moj-module-webapp.git?ref=master"
   product = "${var.product}-${var.microservice}"
   location = "${var.location}"
   env = "${var.env}"
