df8ec7b7285a6bfa9eaaaa38c1c7f3f566683de4
Tim Jacomb <tim.jacomb@hmcts.net>
2018-02-05 15:04:05 +0000

    Test neils staging slot fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 module "pdf-service-api" {
-  source = "git@github.com:contino/moj-module-webapp.git"
+  source = "git@github.com:contino/moj-module-webapp.git?ref=add-java-to-staging-slot"
   product = "${var.product}-${var.microservice}"
   location = "${var.location}"
   env = "${var.env}"
