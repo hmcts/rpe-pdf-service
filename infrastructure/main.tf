36c121ad47c421069e4e4bb64f5b479d706fc982
Tim Jacomb <tim.jacomb@hmcts.net>
2018-04-11 13:27:26 +0100

    Fix ase name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ locals {
   ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
 
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : local.ase_name}"
+  local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.ase_name}"
 
   s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
 }
