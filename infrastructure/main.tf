6e3a6063dafc6603fd5d84b7b09235569aa3cec1
Tim Jacomb <t.jacomb@kainos.com>
2018-04-11 13:37:32 +0100

    ROC-3461 Track dependency calls (#50)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,15 @@
 locals {
   ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
-  s2s_url = "http://rpe-service-auth-provider-${var.env}.service.${local.ase_name}.internal"
+
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+  local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.ase_name}"
+
+  s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
 }
 
 module "pdf-service-api" {
   source = "git@github.com:contino/moj-module-webapp.git?ref=master"
-  product = "${var.product}-${var.microservice}"
+  product = "${var.product}-${var.component}"
   location = "${var.location}"
   env = "${var.env}"
   ilbIp = "${var.ilbIp}"
@@ -16,7 +20,7 @@ module "pdf-service-api" {
 
     ROOT_APPENDER = "CONSOLE"
     REFORM_TEAM = "${var.product}"
-    REFORM_SERVICE_NAME = "${var.microservice}"
+    REFORM_SERVICE_NAME = "${var.component}"
     REFORM_ENVIRONMENT = "${var.env}"
   }
 }
