3d202e54c1844031eb147a405e99b0e11d791c68
Tim Jacomb <t.jacomb@kainos.com>
2018-05-03 10:10:21 +0100

    Add infra local cache (#54)
    Reduce infra recycle time for hardware issues

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,7 +8,7 @@ locals {
 }
 
 module "pdf-service-api" {
-  source = "git@github.com:contino/moj-module-webapp.git?ref=master"
+  source = "git@github.com:hmcts/moj-module-webapp.git?ref=RPE-389/local-cache"
   product = "${var.product}-${var.component}"
   location = "${var.location}"
   env = "${var.env}"
