3f580039e4854d80be5be6d74f02402e279fd772
Tim Jacomb <tim.jacomb@hmcts.net>
2018-09-13 08:56:34 +0100

    Shared ASP

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,6 +5,7 @@ locals {
   local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.ase_name}"
 
   s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
+  sku_size = "${var.env == "prod" || var.env == "sprod" || var.env == "aat" ? "I2" : "I1"}"
 }
 
 module "pdf-service-api" {
@@ -15,6 +16,9 @@ module "pdf-service-api" {
   ilbIp = "${var.ilbIp}"
   subscription = "${var.subscription}"
   common_tags = "${var.common_tags}"
+  asp_name      = "${var.product}-${var.component}-${var.env}"
+  asp_rg        = "${var.product}-${var.component}-${var.env}"
+  instance_size = "${local.sku_size}"
 
   app_settings = {
     S2S_URL = "${local.s2s_url}"
