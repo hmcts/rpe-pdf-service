5cc59a214032415006cd7ee3770582f166b86fc7
Rafal Kondratowicz <rkondr@gmail.com>
2018-11-27 13:15:15 +0000

    Update deps

--- src/main/java/uk/gov/hmcts/reform/pdf/service/exception/ExceptionHandling.java
+++ src/main/java/uk/gov/hmcts/reform/pdf/service/exception/ExceptionHandling.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.reform.pdf.service.exception;
 
-import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -36,11 +34,5 @@ public class ExceptionHandling {
     @ResponseBody
     public void handleMissingAndMalformedParametersValues(Exception exception) {
         log.error("Input parameters were missing/malformed:", exception);
-    }
-
-    @ExceptionHandler(FeignException.class)
-    protected ResponseEntity<Object> handleFeignException(FeignException exc) {
-        log.warn("Error communicating with an API", exc);
-        return ResponseEntity.status(exc.status()).build();
     }
 }
