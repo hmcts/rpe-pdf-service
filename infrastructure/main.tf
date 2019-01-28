b46cec87b33a3e81da13e33405ac29a2f9265cb6
Tim Jacomb <tim.jacomb@hmcts.net>
2019-01-28 07:58:52 +0000

    Remove unused s2s config, and run terraform fmt

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,29 +4,26 @@ locals {
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
   local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.ase_name}"
 
-  s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
   sku_size = "${var.env == "prod" || var.env == "sprod" || var.env == "aat" ? "I2" : "I1"}"
 }
 
 module "pdf-service-api" {
-  source = "git@github.com:hmcts/cnp-module-webapp.git?ref=master"
-  product = "${var.product}-${var.component}"
-  location = "${var.location}"
-  env = "${var.env}"
-  ilbIp = "${var.ilbIp}"
-  subscription = "${var.subscription}"
-  common_tags = "${var.common_tags}"
-  common_tags = "${merge(var.common_tags, map("Team Name", "Software Engineering"))}"
+  source        = "git@github.com:hmcts/cnp-module-webapp.git?ref=master"
+  product       = "${var.product}-${var.component}"
+  location      = "${var.location}"
+  env           = "${var.env}"
+  ilbIp         = "${var.ilbIp}"
+  subscription  = "${var.subscription}"
+  common_tags   = "${var.common_tags}"
+  common_tags   = "${merge(var.common_tags, map("Team Name", "Software Engineering"))}"
   asp_name      = "${var.product}-${var.component}-${var.env}"
   asp_rg        = "${var.product}-${var.component}-${var.env}"
   instance_size = "${local.sku_size}"
 
   app_settings = {
-    S2S_URL = "${local.s2s_url}"
-
-    ROOT_APPENDER = "CONSOLE"
-    REFORM_TEAM = "${var.product}"
+    ROOT_APPENDER       = "CONSOLE"
+    REFORM_TEAM         = "${var.product}"
     REFORM_SERVICE_NAME = "${var.component}"
-    REFORM_ENVIRONMENT = "${var.env}"
+    REFORM_ENVIRONMENT  = "${var.env}"
   }
 }
