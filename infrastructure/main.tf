e205b174f1dfc7f45380f3d190a8470ae5bed601
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-09-09 22:49:01 +0100

    Upgrade terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,27 +1,35 @@
 provider "azurerm" {
-  version = "=1.44.0"
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location}"
+  location = var.location
 
-  tags = "${var.common_tags}"
+  tags = var.common_tags
 }
 
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.component}-appinsights-${var.env}"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
   application_type    = "Web"
 
-  tags = "${var.common_tags}"
+  tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance
+      application_type,
+    ]
+  }
 }
 
 resource "azurerm_key_vault_secret" "AZURE_APPINSGHTS_KEY" {
   name         = "AppInsightsInstrumentationKey"
-  value        = "${azurerm_application_insights.appinsights.instrumentation_key}"
-  key_vault_id = "${data.azurerm_key_vault.rpe_shared_vault.id}"
+  value        = azurerm_application_insights.appinsights.instrumentation_key
+  key_vault_id = data.azurerm_key_vault.rpe_shared_vault.id
 }
 
 data "azurerm_key_vault" "rpe_shared_vault" {
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,22 +1,18 @@
 variable "product" {
-  type = "string"
 }
 
 variable "component" {
-  type = "string"
 }
 
 variable "location" {
-  type    = "string"
   default = "UK South"
 }
 
 variable "env" {
-  type = "string"
 }
 
 variable "subscription" {}
 
 variable "common_tags" {
-  type = "map"
+  type = map(string)
 }
