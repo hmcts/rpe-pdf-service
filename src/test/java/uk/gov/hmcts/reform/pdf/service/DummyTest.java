637d0b65c249285cf1546e4dd5c5c91dd2ca3edd
Tim Jacomb <tim.jacomb@hmcts.net>
2020-10-21 21:23:20 +0100

    [ignore] testing pipeline

---
+++ src/test/java/uk/gov/hmcts/reform/pdf/service/DummyTest.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.pdf.service;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertThat(true).isEqualTo(false);
+    }
+}
