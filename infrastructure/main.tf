403d0ba510267f79fa0d4e032dac94ffae17b897
Jordan Hoey <125922012+JordanHoey96@users.noreply.github.com>
2023-12-12 16:30:13 +0000

    DTSPO-14929 - Migrating App Insights from Classic to Workspace-based. (#699)
    * updating to app insights module
    
    * adding suppresions
    
    * setting daily data cap to match existing config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,26 +9,31 @@ resource "azurerm_resource_group" "rg" {
   tags = var.common_tags
 }
 
-resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-${var.component}-appinsights-${var.env}"
-  location            = var.location
+module "application_insights" {
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+
+  env     = var.env
+  product = var.product
+  name    = "${var.product}-${var.component}-appinsights"
+
   resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
 
-  tags = var.common_tags
+  common_tags = var.common_tags
 
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
-      # destroys and re-creates this appinsights instance
-      application_type,
-    ]
-  }
+  daily_data_cap_in_gb = var.daily_data_cap_in_gb
+  
 }
+
+moved {
+  from = azurerm_application_insights.appinsights
+  to   = module.application_insights.azurerm_application_insights.this
+}
+
+
 
 resource "azurerm_key_vault_secret" "AZURE_APPINSGHTS_KEY" {
   name         = "app-insights-connection-string"
-  value        = azurerm_application_insights.appinsights.connection_string
+  value        = module.application_insights.connection_string
   key_vault_id = data.azurerm_key_vault.rpe_shared_vault.id
 }
 
