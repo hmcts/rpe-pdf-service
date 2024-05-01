56e75f0eefc2285519207451e9bf9633e5ad403d
Ieuan Byers <ieuan.byers@justice.gov.uk>
2024-05-01 16:48:39 +0100

    Fix order of imports

--- src/contractTest/java/uk/gov/hmcts/reform/pdf/service/endpoint/v2/PDFGenerationEndpointV2ProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/pdf/service/endpoint/v2/PDFGenerationEndpointV2ProviderTest.java
@@ -3,11 +3,11 @@ package uk.gov.hmcts.reform.pdf.service.endpoint.v2;
 
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
-import au.com.dius.pact.provider.junitsupport.loader.PactBrokerConsumerVersionSelectors;
 import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.junitsupport.loader.PactBrokerConsumerVersionSelectors;
 import au.com.dius.pact.provider.junitsupport.loader.SelectorBuilder;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import org.json.JSONException;
